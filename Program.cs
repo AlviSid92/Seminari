9e53ef5d0ba546a19424d0d8e4df16e1b0864ec5
dima <alvisid92@gmail.com>
2022-12-15 23:05:25 +0300

    первая задача

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("введите два числа:");
+int a, b;
+Console.WriteLine("Первое число -");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число -");
+b = Convert.ToInt32(Console.ReadLine());
+bool C1 = a > b;
+bool C2 = a == b;
+bool C3 = a < b;
+
+if(C1)
+{
+    Console.WriteLine($"Number {a} > {b}");
+}
+else if(C2)
+{
+    Console.WriteLine($"Number {a} = {b}");
+}
+else
+{ 
+    Console.WriteLine($"Number {a} < {b}");
+}
